6910cb938fa153d17568c3f2b8c086b72deb81f4
Kwaku Amofah-Boafo <boafo819@gmail.com>
2020-07-31 01:57:42 -0400

    additional small UI changes and szing fixes

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ fetch(urlstart + newsEndpoint + key).then(function(response){
         let heading = document.createElement("div");
         heading.className = "card-header";
         heading.innerHTML = data[i].Title;
+        heading.style.fontWeight = "bold";
+        heading.style.color = "#d00";
 
         // this is the content of the card 
         let postBody = document.createElement("div");
@@ -25,13 +27,35 @@ fetch(urlstart + newsEndpoint + key).then(function(response){
         let content = document.createElement("p");
         content.innerHTML = data[i].Content;
         postBody.appendChild(content);
+
+        //this is the interaction menu 
+        let navigation = document.createElement("div");
+        navigation.className = "btn-group";
+        let like = document.createElement("button");
+        let dislike = document.createElement("button");
+        let comment = document.createElement("button");
+        let view = document.createElement("button");
+        like.innerHTML = "Like";
+        dislike.innerHTML = "Dislike";
+        comment.innerHTML = "Comment";
+        view.innerHTML = "View";
+        like.className = "btn btn-primary";
+        dislike.className = "btn btn-primary";
+        comment.className = "btn btn-primary";
+        view.className = "btn btn-primary";
+        navigation.appendChild(like);
+        navigation.appendChild(dislike);
+        navigation.appendChild(comment);
+        navigation.appendChild(view);
         
         // this code puts all the pieces of the card together 
         // and spaces them out appropriately 
         post.appendChild(heading);
         post.appendChild(postBody);
+        post.appendChild(navigation);
         let feed = document.getElementById("feedView");
         feed.appendChild(post);
+        post.style.width = "800px";
         post.style.marginLeft = "20px";
         post.style.marginRight = "20px";
         post.style.marginBottom = "20px";
