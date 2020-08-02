910666dba77b1579dc63c10acb7443845a78aa75
Kwaku Amofah-Boafo <boafo819@gmail.com>
2020-08-01 23:55:07 -0400

    Tonight's Leaders Feature Now Working

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,31 @@
+
+//Url and Endpoints
 let urlstart = "https://api.sportsdata.io/v3/nba/stats/json/";
 let key = "?key=04342f2525124fc8967646d8bfc8826e";
 let newsEndpoint = "News";
+let playerStatsForDate = "PlayerGameStatsByDate/";
+
+// gets the date for the endpoint
+let today = new Date();
+let year = today.getFullYear();
+let month = today.getMonth();
+let monthsArr = ["JAN" , "FEB", "MAR", "APR" , "MAY" , "JUN" , "JUL" , "AUG", "SEP" , "OCT", "NOV", "DEV"];
+let monthStr = monthsArr[month -1];
+let previousDay = new Date(today.setDate(today.getDate() - 1));
+let previousDayNum = previousDay.getDate(); 
+let dateEndpoint = `${year}-${monthStr}-${previousDayNum}`;
+
+// Variables for Tonights Leaders Section
+let points = 0;
+let assists = 0;
+let rebounds = 0;
+let steals = 0;
+let blocks = 0;
+let pointsLead = "";
+let assistsLead = "";
+let rebsLead = "";
+let stealsLead = "";
+let blocksLead = "";
 
 
 // this code fetchs sports news (currently only nba news) and populates the feed
@@ -63,3 +88,49 @@ fetch(urlstart + newsEndpoint + key).then(function(response){
     }
 });
 
+
+fetch(urlstart + playerStatsForDate + dateEndpoint + key).then(function(response){
+    return response.json();
+}).then(function(data){
+    for(let i = 0; i < data.length -1; i++){
+        if(data[i].Points > points){
+            points = data[i].Points;
+            pointsLead = data[i].Name;
+        }
+        if(data[i].Assists > assists){
+            assists = data[i].Assists;
+            assistsLead = data[i].Name;
+        }
+        if(data[i].Rebounds > rebounds){
+            rebounds = data[i].Rebounds;
+            rebsLead = data[i].Name;
+        }
+        if(data[i].Steals > steals){
+            steals = data[i].Steals;
+            stealsLead = data[i].Name;
+        }
+        if(data[i].Blocks > blocks){
+            blocks = data[i].Blocks;
+            blocksLead = data[i].Name;
+        }
+    
+    }
+    console.log(pointsLead);
+    console.log(assistsLead);
+    console.log(rebsLead);
+    console.log(stealsLead);
+    console.log(blocksLead);
+
+    let PL = document.getElementById("pointsLeader");
+    let AL = document.getElementById("assistsLeader");
+    let RL = document.getElementById("reboundsLeader");
+    let SL = document.getElementById("stealsLeader");
+    let BL = document.getElementById("blocksLeader");
+ 
+    PL.innerHTML = pointsLead;
+    AL.innerHTML = assistsLead;
+    RL.innerHTML = rebsLead;
+    SL.innerHTML = stealsLead;
+    BL.innerHTML = blocksLead;
+    
+});
